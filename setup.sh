5bdb729ae80d33b7c1890df794e6d4ae7749c59a
Yash Khare <yashsja@gmail.com>
2021-07-12 01:56:15 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install tmux
+sudo apt-get install python3-pip
+sudo apt-get install vim
+pip install ai-economist
+pip install ray[rllib]
+git clone https://github.com/akirasosa/aie-train.git
+python aie-train/src/train_free_market.py 
